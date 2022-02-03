2251487668ef2dea1d9bd8d3749b3de97e4c4dfe
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2022-02-02 23:14:44 -0300

    Adiciona parte visual da Home

--- meulink/src/pages/Home/index.js
+++ meulink/src/pages/Home/index.js
@@ -1,8 +1,24 @@
+import {FiLink} from 'react-icons/fi';
+import './home.css'
 
 export default function Home(){
     return(
-        <div>
-        <h1>Página Home</h1>
+        <div className="container-home">
+            <div className="logo">
+                <img src="/logo.svg" alt="Sujeito Link Logo" />
+                <h1>Sujeito Link</h1>
+                <span>Cole seu link para encurtar 👇</span>
+            </div>
+
+            <div className="area-input">
+                <div>
+                    <FiLink size={24} color="#FFF"/>
+                    <input  
+                        placeholder="Cole seu link aqui..."
+                    />
+                </div>
+                <button>Encurtar Link</button>
+            </div>
         </div>
     )
 }
